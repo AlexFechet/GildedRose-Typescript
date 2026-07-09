438b3fb9d6bd7564c601451ca93f4c3f6b02121c
Alex Fechet <alex.fechet@softwire.com>
2026-07-09 14:08:49 +0300

    refactor for each branch to represent an object

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -36,31 +36,19 @@ export class GildedRose {
                 } else {
                     this.items[i].quality = this.items[i].quality + 1
                 }
-                if(this.items[i].quality > 50) {
-                    this.items[i].quality = 50;
-                }
-                continue;
-            }
-
-            if (this.items[i].name === 'Aged Brie') {
+            } else if (this.items[i].name === 'Aged Brie') {
                 if (this.items[i].sellIn < 0) {
                     this.items[i].quality = this.items[i].quality + 2
                 } else {
                     this.items[i].quality = this.items[i].quality + 1
                 }
-                if(this.items[i].quality > 50) {
-                    this.items[i].quality = 50;
+            } else if (this.items[i].name === "Conjured Mana Cake") {
+                if (this.items[i].sellIn < 0) {
+                    this.items[i].quality = this.items[i].quality - 4
+                } else {
+                    this.items[i].quality = this.items[i].quality - 2
                 }
-                continue;
-            }
-
-            if (this.items[i].sellIn < 0) {
-                this.items[i].quality = this.items[i].quality - 2
             } else {
-                this.items[i].quality = this.items[i].quality - 1
-            }
-
-            if (this.items[i].name === "Conjured Mana Cake") {
                 if (this.items[i].sellIn < 0) {
                     this.items[i].quality = this.items[i].quality - 2
                 } else {
@@ -68,10 +56,12 @@ export class GildedRose {
                 }
             }
 
+            if(this.items[i].quality > 50) {
+                this.items[i].quality = 50;
+            }
             if(this.items[i].quality < 0) {
                 this.items[i].quality = 0;
             }
-
         }
 
         return this.items;
